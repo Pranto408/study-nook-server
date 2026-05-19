3b06699ca2d202d9e5af7e6bfed0cadcac43b860
Pranto408 <prantodatta408@gmail.com>
2026-05-19 23:51:15 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,67 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const cors = require("cors");
+dotenv.config();
+const app = express();
+app.use(cors());
+const port =process.env.PORT || 8080;
+
+// studynook
+// quE7iZ6vWu0VwdS2
+
+
+
+const uri =
+  "mongodb+srv://studynook:quE7iZ6vWu0VwdS2@cluster0.fepo71s.mongodb.net/?appName=Cluster0";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+      // await client.db("admin").command({ ping: 1 });
+      const db = client.db("studynookdb");
+      const roomsCollection = db.collection("rooms")
+      
+      app.get("/rooms", async (req, res) => {
+          const cursor = roomsCollection.find();
+          const result = await cursor.toArray();
+          res.send(result);
+      })
+      app.get("/rooms/:roomId", async (req, res) => {
+          const { roomId } = req.params;
+          const query = { _id: new ObjectId(roomId) }
+          const result = await roomsCollection.findOne(query);
+          res.send(result);
+      });
+
+
+
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!",
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
